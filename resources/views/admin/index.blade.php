f07fb25f1a62e47af0dd8e9b74b4149d6207b09d
Oussama Louhaibi <oussamalouhaibi@gmail.com>
2018-07-20 13:29:30 +0100

    create admin folder

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('title')
+
+    Admin Page
+
+@endsection
+
+@section('content')
+    
+    <div class="m-grid__item m-grid__item--fluid m-wrapper">
+        
+    </div>
+
+@endsection 
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,45 +1,69 @@
 <!DOCTYPE html>
 
 
-<html lang="en" >
-	<!-- begin::Head -->
-	<head>
-		<meta charset="utf-8" />
-		<title>
-			Metronic | My Profile
-		</title>
-		<meta name="description" content="User profile view and edit">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<!--begin::Web font -->
-		<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
-		<script>
-          WebFont.load({
-            google: {"families":["Poppins:300,400,500,600,700","Roboto:300,400,500,600,700"]},
-            active: function() {
+<html lang="en">
+<!-- begin::Head -->
+
+<head>
+    <meta charset="utf-8" />
+    <title>
+        Metronic | @yield('title')
+    </title>
+    <meta name="description" content="User profile view and edit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--begin::Web font -->
+    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
+    <script>
+        WebFont.load({
+            google: {
+                "families": ["Poppins:300,400,500,600,700", "Roboto:300,400,500,600,700"]
+            },
+            active: function () {
                 sessionStorage.fonts = true;
             }
-          });
-		</script>
-		<!--end::Web font -->
-        <!--begin::Base Styles -->
-		<link href="{{ asset('assets/vendors/base/vendors.bundle.css') }}" rel="stylesheet" type="text/css" />
-		<link href="{{ asset('assets/demo/demo12/base/style.bundle.css') }} " rel="stylesheet" type="text/css" />
-		<!--end::Base Styles -->
-		<link rel="shortcut icon" href="{{ asset('assets/demo/demo12/media/img/logo/favicon.ico') }}" />
-	</head>
-	<!-- end::Head -->
-    <!-- end::Body -->
-	<body  class="m-page--fluid m--skin- m-content--skin-light2 m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default"  >
-		<div class="m-grid m-grid--hor m-grid--root m-page">
+        });
+
+    </script>
+    <!--end::Web font -->
+    <!--begin::Base Styles -->
+    <link href="{{ asset('assets/vendors/base/vendors.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/demo/demo12/base/style.bundle.css') }} " rel="stylesheet" type="text/css" />
+    <!--end::Base Styles -->
+    <link rel="shortcut icon" href="{{ asset('assets/demo/demo12/media/img/logo/favicon.ico') }}" />
+</head>
+<!-- end::Head -->
+<!-- end::Body -->
+
+<body class="m-page--fluid m--skin- m-content--skin-light2 m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default">
+    <div class="m-grid m-grid--hor m-grid--root m-page">
+
+		@include('partials.navbar') 
 		
-			<!-- begin:: Page -->
-				@include('partials.navbar') 
-				@include('partials.aside_menu')
-				@include('partials.footer')
-			<!--end::Base Scripts -->
+		<div class="m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-body">
+			
+			@include('partials.aside_menu') 
+			@yield('content')
+
+		</div> 
 		
-	</div>
-	</body>
-	<!-- end::Body -->
+		@include('partials.footer')
+
+    </div>
+
+
+    <!-- begin::Quick Nav -->
+    <!--begin::Base Scripts -->
+    <script src="assets/vendors/base/vendors.bundle.js" type="text/javascript"></script>
+    <script src="assets/demo/demo12/base/scripts.bundle.js" type="text/javascript"></script>
+    <!--end::Base Scripts -->
+    <!--begin::Page Vendors -->
+    <script src="assets/vendors/custom/fullcalendar/fullcalendar.bundle.js" type="text/javascript"></script>
+    <!--end::Page Vendors -->
+    <!--begin::Page Snippets -->
+    <script src="assets/app/js/dashboard.js" type="text/javascript"></script>
+    <!--end::Page Snippets -->
+</body>
+<!-- end::Body -->
+
 </html>
